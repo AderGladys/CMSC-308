54469f9eec0f18e36c684aa49ebe02659f2406b7
AderGladys <102132079+AderGladys@users.noreply.github.com>
2022-10-18 03:50:19 -0700

    Create program.cs

---
+++ Activity6/program.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CodeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<char> charList = new List<char>(10) {'S', 'X', 'C', 'O', 'M', 'P', 'U', 'T', 'E', 'R'};
+
+            Console.Write("Enter code: ");
+            string code = Console.ReadLine().ToUpper();
+            char[] charInput = code.ToCharArray();
+
+            Console.Write("Equivalent Number: ");
+
+            foreach (char c in charInput)
+            {
+                int num = charList.IndexOf(c);
+                if(num >= 0)
+                {
+                    Console.Write(num);
+                }
+                else
+                {
+                    Console.Write(" err ");
+                }
+            }
+        }
+    }
+}
